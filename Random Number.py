1537eb5ab1c1c846f4263cf03367953533fec4cd
Spedymax <sokolov.maksim10@gmail.com>
2020-05-01 12:28:49 +0300

    Modified this file

--- Random Number.py
+++ Random Number.py
@@ -1,12 +1,26 @@
 import random
 
 
-def random_number():
-    number = input('Нажмите Enter чтобы крутить кубик: ')
-    print(number)
-    if number == "":
-        rand_number = random.randint(1, 6)
-        print(rand_number)
+my_list = []
+imovirnist = []
+n = 0
+o = 0
+i = 0
+number5 = 0
+while n < 6:
+    while i < 6:
+        number = random.randint(1, 6)
+        my_list.append(number)
+        if number == 5:
+            number5 += 1
+        i += 1
+        imovirnist.append(number5 / len(my_list))
 
+    n += 1
+p = 2
+imovirnist2 = imovirnist[p]
+print(my_list)
+print(number5)
+print(imovirnist)
+print(imovirnist2)
 
-random_number()
